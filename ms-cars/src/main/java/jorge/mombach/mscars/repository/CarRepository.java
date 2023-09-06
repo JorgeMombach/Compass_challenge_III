69cf15826f3977fb274218f3dea6f3c16ca03988
JamaykonCZ <jorge.mombach@gmail.com>
2023-09-06 17:13:20 -0300

    ms-cars/feature01-crudCars: Fixed response status in the exceptions

--- ms-cars/src/main/java/jorge/mombach/mscars/repository/CarRepository.java
+++ ms-cars/src/main/java/jorge/mombach/mscars/repository/CarRepository.java
@@ -2,7 +2,6 @@ package jorge.mombach.mscars.repository;
 
 import jorge.mombach.mscars.entity.Car;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Date;
